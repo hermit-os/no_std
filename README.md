29f65026fe88b35297900fcea5730454b6f80019
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-03-15 07:15:58 +0100

    add draft to bool the HermitOS without std

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # no_std
+
+`no_std` allows you  to boot HermitOS without `std`.
+Please not, the kernel interface is not stable and is under active development! 
+Only `std` has a stable interface.
+
+## Requirements
+
+* [`rustup`](https://www.rust-lang.org/tools/install)
+
+## Building the kernel
+
+```sh
+cargo build
+```
+
+## Booting the kernel
+
+Boot kernel with `cargo run` or download the (`hermit-loader-x86_64`)[https://github.com/hermit-os/loader/releases] use following command:
+
+```sh
+qemu-system-x86_64 -display none -serial stdio -kernel hermit-loader-x86_64 -cpu Skylake-Client -device isa-debug-exit,iobase=0xf4,iosize=0x04 -smp 1 -m 512M -netdev user,id=u1,hostfwd=tcp::9975-:9975,hostfwd=udp::9975-:9975,net=192.168.76.0/24,dhcpstart=192.168.76.9 -device virtio-net-pci,netdev=u1,disable-legacy=on -initrd target/x86_64-unknown-none/debug/no_std
+```
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+
+The kernel is being developed on [hermit-os/kernel](https://github.com/hermit-os/kernel).
+Create your own fork, send us a pull request, and chat with us on [Zulip](https://hermit.zulipchat.com/).
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+#![no_std] // don't link the Rust standard library
+#![no_main]
+
+#[macro_use]
+extern crate log;
+extern crate alloc;
+extern crate hermit;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use core::ffi::CStr;
+use hermit::fs::{self, readdir, File};
+use hermit::io::{Read, Write};
+use hermit::{sys_close, sys_getdents64, sys_opendir, sys_shutdown, Dirent64};
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn main(_argc: i32, _argv: *const *const u8, _env: *const *const u8) {
+    info!("Enter main function");
+
+    info!("Read content of / with");
+    /*for i in readdir("/").expect("Unable to read /").iter() {
+        info!("{:?}", *i);
+    }*/
+
+    let fd = sys_opendir("/\0".as_ptr());
+    let mut v: Vec<u8> = Vec::new();
+    v.resize(0x1000, 0);
+    let readlen = sys_getdents64(fd, v.as_mut_ptr() as *mut Dirent64, 0x1000);
+    let mut i = 0;
+    loop {
+        if i >= readlen {
+            break;
+        }
+
+        let dir = unsafe { &*(v.as_ptr().offset(i.try_into().unwrap()) as *const Dirent64) };
+        let name = unsafe {
+            CStr::from_ptr(&dir.d_name as *const _ as *const i8)
+                .to_str()
+                .unwrap()
+        };
+        info!("{}", name);
+        i = i + dir.d_off;
+    }
+    sys_close(fd);
+
+    info!("Read content of /proc with");
+    for i in readdir("/proc").expect("Unable to read /proc").iter() {
+        info!("{:?}", *i);
+    }
+
+    if let Ok(attr) = fs::file_attributes("/proc/version") {
+        info!("Attributes of /etc/version {:?}", attr);
+    } else {
+        error!("Unable to get file attributes");
+    }
+
+    if let Ok(mut file) = File::open("/proc/version") {
+        let mut version: String = String::new();
+        if file.read_to_string(&mut version).is_err() {
+            error!("Unable to read /proc/version");
+        }
+        info!("version: {}", version);
+    } else {
+        error!("Unable to open file");
+    };
+
+    info!("create file /host/test.txt");
+    if let Ok(mut file) = File::create("/host/test.txt") {
+        write!(file, "Hello Linux!").expect("Unable to write into /host/test.txt");
+    } else {
+        error!("Unable to create file");
+    }
+
+    info!("read file /host/test.txt");
+    if let Ok(mut file) = File::open("/host/test.txt") {
+        let mut content: String = String::new();
+        if file.read_to_string(&mut content).is_err() {
+            error!("Unable to read /proc/version");
+        }
+        info!("File content {}", content);
+    } else {
+        error!("Unable to open file");
+    }
+
+    sys_shutdown(0);
+}
